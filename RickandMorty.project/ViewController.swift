b40c8d589eaa06782b2dcad2cf689cb3b8d4b008
gn <ngng>
2023-09-23 13:09:53 +0600

    Initial Commit

---
+++ RickandMorty.project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMorty.project
+//
+//  Created by Islam  EU on 23/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
